3ca52dd362265ff555bb502817d23b9b1b3e9855
izzybogart <60237226+izzybogart@users.noreply.github.com>
2021-03-04 13:30:59 -0700

    Create HourlyEmployee.hpp

---
+++ HourlyEmployee.hpp
@@ -0,0 +1,30 @@
+#ifdef HOURLYEMPLOYEE_H
+#define HOURLYEMPLOYEE_H
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "employee.hpp"
+
+class HourlyEmployee : public Employee {
+        public:
+                HourlyEmployee(double hours, double rates, double overtime);    //constructor
+        //set functions
+                void setHoursWorked(double hours);
+                void setHourlyRate(double rates);
+                void setOvertimeHours(double overtime);
+        //get functions
+                double getHoursWorked();
+                double getHourlyRate();
+                double getOvertimeHours();
+        //other functions
+                void calcHourlySalary();
+                void printHourly();
+
+        private:
+                double HoursWorked;
+                double HourlyRate;
+                double OvertimeHours;
+};
+
+#endif
