f58c4ff48595eb07b7d55dafc0cd4985438351a5
izzybogart <60237226+izzybogart@users.noreply.github.com>
2021-03-04 13:32:56 -0700

    Create SalariedEmployee.hpp

---
+++ SalariedEmployee.hpp
@@ -0,0 +1,24 @@
+#ifdef SALARIEDEMPLOYEE_H
+#define SALARIEDEMPLOYEE_H
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "employee.hpp"
+
+class SalariedEmployee : public Employee {
+        public:
+                SalariedEmployee(double salary, int months);    //constructor
+                void setSalary(double salary);                  //set salalry
+                void setMonths(int months);                     //set months worked
+                double getSalary() const;                       //get salary
+                int getMonths() const;                          //get months worked
+                void calcSalary();
+                void printSalary();
+
+        private:
+                double monthlySalary;
+                int monthsWorked;
+};
+
+#endif
